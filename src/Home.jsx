63d5f89c942992bae991171ee194fe85e8920d2e
Antonio Juevan <86933408+Juevan@users.noreply.github.com>
2023-01-18 00:23:17 -0300

    Add files via upload

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,19 @@
-import style from './Home.module.css'
+import style from './Home.css'
 export default function Home(){
     return(
-        <div className={style.fodase}>
-            <p>olá</p>
+        <div className='ContainerMain'>
+            <div className='ContainerPass'>
+                <h1>Login</h1>
+                <br />
+                <h4>Usuario</h4>
+                <input type="text" name="" id="us" />
+                <h4>Senha</h4>
+                <input type="password" name="" id="pass" />
+                <div>
+                    <input type="button" value="Login" id='btn'/>
+                    <select name="" id="slt"></select>
+                </div>
+            </div>
         </div>
     )
 }
