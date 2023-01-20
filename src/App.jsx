3937f333ecd1e60e53c5ecd3810710f0c9c655eb
Antonio Juevan <86933408+Juevan@users.noreply.github.com>
2023-01-20 20:00:19 -0300

    nova página de login
    nova página de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,45 @@
 import { useState } from 'react'
 import './App.css'
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home.jsx';
 import Contacts from './Contacts.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
+
+
   return (
     <BrowserRouter>
-      <header>
-          <main>
-            <img src="#" alt="" />
-            <input type="text" />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Contacts' element={<Contacts />} />
+      </Routes>
+      <div className='mainContainer'>
+        <div className='contaner'>
+          <main class="form-signin w-100 m-auto">
+            <form>
+              <img class="mb-4" src="https://i.pinimg.com/originals/5c/00/e5/5c00e519ad324b8ae46e6c91bd3bdecc.jpg" alt="" width="72" height="57" />
+              <h1 class="h3 mb-3 fw-normal">Inicie para continuar</h1>
+              <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Usuário</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Senha</label>
+              </div>
+              <div class="checkbox mb-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Administrador
+                </label>
+              </div>
+              <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar</button>
+              <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+            </form>
           </main>
-          <footer>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/Contacts'>Contato</Link>
-              </li>
-            </ul>
-          </footer>
-      </header>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Contacts' element={<Contacts/>}/>
-    </Routes>
-      <footer className='footermain'>
-        <div>
-          <h3>
-            Categorias
-          </h3>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Contacts'>Contato</Link>
-            </li>
-          </ul>
         </div>
-        <div>
-          <h3>
-            Categorias
-          </h3>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Contacts'>Contato</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3>
-            Categorias
-          </h3>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Contacts'>Contato</Link>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3>
-            Categorias
-          </h3>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/Contacts'>Contato</Link>
-            </li>
-          </ul>
-        </div>
-      </footer>
+      </div>
     </BrowserRouter>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,8 @@
-import style from './Home.css'
-export default function Home(){
-    return(
-        <div className='ContainerMain'>
-            <div className='ContainerPass'>
-                <h1>Login</h1>
-                <br />
-                <h4>Usuario</h4>
-                <input type="text" name="" id="us" />
-                <h4>Senha</h4>
-                <input type="password" name="" id="pass" />
-                <div>
-                    <input type="button" value="Login" id='btn'/>
-                    <select name="" id="slt">
-                        <option disabled selected value="">Tipo de Acesso</option>
-                        <option value="">Adiministrador</option>
-                        <option value="">Operacional</option>
-                    </select>
-                </div>
-            </div>
-        </div>
+import './Home.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+export default function Home() {
+    return (
+        <div></div>
     )
 }
